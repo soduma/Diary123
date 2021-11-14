d79f9992ad70850be5711a01fb80e1ab8517d1eb
soduma <soduma@naver.com>
2021-11-14 16:42:12 +0900

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by 장기화 on 2021/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
