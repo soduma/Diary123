d2473d3e0b7dc0bb18617d844dc039c5ebc937bd
soduma <soduma@naver.com>
2021-11-14 18:39:57 +0900

    입력 검증 코드

--- Diary/WriteDiaryViewController.swift
+++ Diary/WriteDiaryViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class WriteDiaryViewController: UIViewController {
 
-    @IBOutlet var titleTextView: UITextField!
+    @IBOutlet var titleTextField: UITextField!
     @IBOutlet var contentsTextView: UITextView!
     @IBOutlet var dateTextField: UITextField!
     @IBOutlet var confirmButton: UIBarButtonItem!
@@ -20,8 +20,15 @@ class WriteDiaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureInputField()
         configureContentsTextView()
         configureDatePicker()
+        confirmButton.isEnabled = false
+    }
+    
+    //유저가 화면을 터치하면 호출되는 메서드!!
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     private func configureContentsTextView() {
@@ -35,7 +42,18 @@ class WriteDiaryViewController: UIViewController {
         datePicker.datePickerMode = .date
         datePicker.preferredDatePickerStyle = .wheels
         datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        datePicker.locale = Locale(identifier: "ko_KR")
         dateTextField.inputView = datePicker
+    }
+    
+    private func configureInputField() {
+        contentsTextView.delegate = self
+        titleTextField.addTarget(self, action: #selector(titleTextFieldDidChange(_:)), for: .editingChanged)
+        dateTextField.addTarget(self, action: #selector(dateTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func validateInputField() {
+        confirmButton.isEnabled = !(titleTextField.text?.isEmpty ?? true) && !(contentsTextView.text.isEmpty ) && !(dateTextField.text?.isEmpty ?? true)
     }
     
     @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
@@ -44,10 +62,27 @@ class WriteDiaryViewController: UIViewController {
         formatter.locale = Locale(identifier: "ko_KR")
         diaryDate = datePicker.date
         dateTextField.text = formatter.string(from: datePicker.date)
+        dateTextField.sendActions(for: .editingChanged)
     }
+    
+    @objc private func titleTextFieldDidChange(_ textField: UITextField) {
+        validateInputField()
+    }
+    
+    @objc private func dateTextFieldDidChange(_ textField: UITextField) {
+        validateInputField()
+    }
+    
     @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
     }
     
 
 
+}
+
+
+extension WriteDiaryViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        validateInputField()
+    }
 }
